0d60b7920609ee6b484ed6282e5ce4aab796df94
SergeyVolik <volik.sergey112@gmail.com>
2022-01-25 09:49:30 +0200

    refactor: paths to scripts

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,10 @@
+﻿namespace Prototype
+{
+
+    public enum Scenes
+    {
+        MainScene = 0,
+        StressTestScene = 1,
+        SmallScene = 2
+    }
+}
---
+++ Assets/Scripts/UI/OpenSceneButtonEvent.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Prototype
+{
+
+    [RequireComponent(typeof(Button))]
+    public class OpenSceneButtonEvent : MonoBehaviour
+    {
+       
+        [SerializeField] private Scenes m_Scene;
+
+        private Button m_Button;
+
+        private void Awake()
+        {
+            m_Button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            m_Button.onClick.AddListener(ChangeScene);
+        }
+
+        private void OnDisable()
+        {
+            m_Button.onClick.RemoveListener(ChangeScene);
+        }
+
+        private void ChangeScene()
+        {
+            SceneManager.LoadScene((int)m_Scene);
+        }
+    }
+
+}
